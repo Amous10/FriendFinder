4d05e7f8dae1551b940299f1a15979313da228ff
Amy Merrill <amous.fullstack@gmail.com>
2019-05-13 14:44:52 -0700

    add friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,28 @@
 var friends = [{
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Jen",
+        "photo": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?cs=srgb&dl=beautiful-brunette-cute-774909.jpg&fm=jpg",
         "scores": [
             5,
             1,
             4,
             4,
             5,
+            5,
+            2,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Lyla",
+        "photo": "https://www.pexels.com/photo/brown-haired-woman-wearing-brown-blazer-focus-photo-712521/",
+        "scores": [
+            4,
+            1,
+            4,
+            4,
+            3,
             1,
             2,
             5,
@@ -15,33 +31,65 @@ var friends = [{
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Ellie",
+        "photo": "https://www.pexels.com/photo/woman-sitting-and-smiling-1858175/",
         "scores": [
             5,
             1,
-            4,
+            1,
             4,
             5,
             1,
             2,
+            1,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Cole",
+        "photo": "https://www.pexels.com/photo/man-sitting-on-bench-having-a-cup-of-coffee-374044/",
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            5,
+            5,
+            5,
             5,
             4,
             1
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Jake",
+        "photo": "https://www.pexels.com/photo/man-in-red-crew-neck-sweatshirt-photography-941693/",
         "scores": [
             5,
             1,
+            3,
             4,
+            2,
+            1,
+            3,
+            5,
             4,
+            5
+        ]
+    },
+    {
+        "name": "Thomas",
+        "photo": "https://www.pexels.com/photo/man-wearing-gray-and-red-crew-neck-shirt-smiling-1121796/",
+        "scores": [
             5,
             1,
+            1,
+            1,
             2,
-            5,
+            1,
+            2,
+            3,
             4,
             1
         ]
